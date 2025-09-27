1062db136402e1f41ee2532041c78273d15109bc
Khushi Sarawagi <khushi.24bcs10361@sst.scaler.com>
2025-09-27 19:49:54 +0530

    a

---
+++ DeckBirds/Assets/Scripts/Bird.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float maxDragDistance = 2;
+    // Start is called before the first frame update
+    [SerializeField] float launchPower = 350;
+    LineRenderer lineRenderer;
+    Vector3 startingPosition;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.enabled = false;
+        startingPosition = transform.position;
+    }
+
+    void OnMouseUp()
+    {
+        Vector3 directionAndMagnitude = startingPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionAndMagnitude * launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        lineRenderer.enabled = false;
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        destination.z = 0;
+        if (Vector2.Distance(destination, startingPosition) > maxDragDistance)
+        {
+            destination = Vector3.MoveTowards(startingPosition, destination, maxDragDistance);
+        }
+
+        transform.position = destination;
+        lineRenderer.SetPosition(1, transform.position);
+        lineRenderer.enabled = true;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+
+        if(FindAnyObjectByType<Enemy>(FindObjectsInactive.Exclude)== null )
+        {
+            Debug.Log("You win");
+            int levelToLoad = SceneManager.GetActiveScene().buildIndex+1;
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Invoke(nameof(ReloadLevel),5);
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
